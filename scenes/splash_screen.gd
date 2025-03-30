25f04a519f86abc66d9a8a177d11fafa5a13eef0
sarcocode <sarco4you@gmail.com>
2025-03-30 22:08:44 +0300

    Дизайн
    Добавил качественного дизайна, чтобы глаза не выпадали, когда смотришь на это говнище, только вот этот ебучий шрифт не поддерживает цифры, поэтому количество жизней не отображается, а я это заметил только что XDDDD

--- scenes/splash_screen.gd
+++ scenes/splash_screen.gd
@@ -1,6 +1,10 @@
 extends Control
 
+# Время показа сплеш-скрина в секундах
+const SHOW_TIME = 3.0
+
 func _ready():
-	# Ждем 3 секунды, затем переходим в главное меню
-	await get_tree().create_timer(3.0).timeout
+	# Создаём таймер на 3 секунды
+	await get_tree().create_timer(SHOW_TIME).timeout
+	# Переходим в главное меню после таймера
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
